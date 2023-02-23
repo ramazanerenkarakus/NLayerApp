e85366c5607e19a1d9128a6979d47bf161e7ff07
Ramazan Eren Karakus <ramazanerenkarakus@gmail.com>
2023-02-23 18:19:54 +0300

    Some corrections and additions have been made.

---
+++ NLayer.Service/Services/ProductService.cs
@@ -0,0 +1,25 @@
+﻿using NLayer.Core.DTOs;
+using NLayer.Core.Models;
+using NLayer.Core.Repositories;
+using NLayer.Core.Service;
+using NLayer.Core.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Service.Services
+{
+    public class ProductService : Service<Product>, IProductService
+    {
+        public ProductService(IGenericRepository<Product> repository, IUnitOfWork unitOfWork) : base(repository, unitOfWork)
+        {
+        }
+
+        public Task<List<ProductWithCategoryDto>> GetProductsWithCategory()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
