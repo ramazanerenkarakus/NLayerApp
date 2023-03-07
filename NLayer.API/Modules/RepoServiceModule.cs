f5e2eb11fe0f1c7e2ea507dee12367458dd1033d
Ramazan Eren Karakus <ramazanerenkarakus@gmail.com>
2023-03-07 04:57:49 +0300

    Some regulations in Caching Layer.

--- NLayer.API/Modules/RepoServiceModule.cs
+++ NLayer.API/Modules/RepoServiceModule.cs
@@ -1,4 +1,5 @@
 ﻿using Autofac;
+using NLayer.Caching;
 using NLayer.Core.Repositories;
 using NLayer.Core.Service;
 using NLayer.Core.UnitOfWorks;
@@ -32,6 +33,9 @@ namespace NLayer.API.Modules
             builder.RegisterAssemblyTypes(apiAssembly, repoAssembly, serviceAssembly).Where(x => x.Name.EndsWith("Repository")).AsImplementedInterfaces().InstancePerLifetimeScope();
 
             builder.RegisterAssemblyTypes(apiAssembly, repoAssembly, serviceAssembly).Where(x => x.Name.EndsWith("Service")).AsImplementedInterfaces().InstancePerLifetimeScope();
+
+
+            builder.RegisterType<ProductServiceWithCaching>().As<IProductService>();
 
 
 
--- NLayer.Caching/ProductServiceWithCaching.cs
+++ NLayer.Caching/ProductServiceWithCaching.cs
@@ -40,11 +40,8 @@ namespace NLayer.Caching
 
             if (!_memoryCache.TryGetValue(CacheProductKey, out _))
             {
-                _memoryCache.Set(CacheProductKey, _repository.GetProductsWithCategory());
+                _memoryCache.Set(CacheProductKey, _repository.GetProductsWithCategory().Result);
             }
-
-
-
 
         }
 
