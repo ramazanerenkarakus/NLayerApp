cf894f2947155e91d0567f8112cf8ca75ada1bc4
Ramazan Eren Karakus <ramazanerenkarakus@gmail.com>
2023-03-11 17:00:15 +0300

    MVC NotFoundFilter added.

--- NLayer.Web/Controllers/ProductsController.cs
+++ NLayer.Web/Controllers/ProductsController.cs
@@ -48,6 +48,8 @@ namespace NLayer.Web.Controllers
             return View();
         }
 
+
+        [ServiceFilter(typeof(NotFoundFilter<Product>))]
         public async Task<IActionResult> Update(int id)
         {
             var product = await _sercives.GetByIdAsync(id);
@@ -70,6 +72,17 @@ namespace NLayer.Web.Controllers
             ViewBag.categories = new SelectList(categoriesDto, "Id", "Name", productDto.CategoryId);
 
             return View(productDto);
+        }
+
+
+        public async Task<IActionResult> Remove(int id)
+        {
+            var product = await _sercives.GetByIdAsync(id);
+
+            await _sercives.RemoveAsync(product);
+
+            return RedirectToAction(nameof(Index));
+
         }
 
     }
